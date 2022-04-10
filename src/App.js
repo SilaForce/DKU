0023a73848190dc1e5610e35b258d9787bc12596
Amar Silajdzic <90404958+SilaForce@users.noreply.github.com>
2022-04-10 14:17:04 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./screens/dashboard/index";
+import Project from "./screens/project/project";
+import MakeProject from "./screens/makeProject/index";
+import imageHappy from "./assets/images/happy-image.png";
+
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/project" element={<Project />} />
+                <Route path="/make-project" element={<MakeProject />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default App;
+
+/*<Project
+                    name="Humanitarni bazar"
+                    desProj="Humanitarni bazar za prikupljanje novca za Crveni Križ Ilijaš"
+                    active={true}
+                    location="Ilijaš"
+                    type={['Indirektno', 'Zagovaranje']}
+                    organizer={{
+                        name: "Carpe diem",
+                        description: "UDRUŽENJE OMLADINSKA ORGANIZACIJA 'ISKORISTI DAN - CARPE DIEM' je osnovana sa ciljem ostvarivanja zajedničkih i općih interesa kao i osnaživanja mladih",
+                        logo: imageHappy,
+                        link: "https://www.facebook.com/omladinskaorganizacijacarpediem/",
+                        telephone:"060 32 06 605",
+                        email:"oocarpediem14@outlook.com"
+                    }}
+                    date="22.4.2022"
+                    time="10:00-17:00"
+                    hoursToGet="7"
+                    volNeeded="25"
+                    image={imageHappy}
+
+                />*/
